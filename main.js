2caf90177acfb3490069821296f8188d17d96df0
Cennge <ilyagliba02@gmail.com>
2024-12-06 12:25:54 +0300

    added styles arr

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let styles = [
-    { name: "color", value: "blue" },
-    { name: "font-size", value: "24px" },
-    { name: "text-align", value: "center" },
-    { name: "text-decoration", value: "underline" },
-    { name: "background-color", value: "lightgrey" }
-];
-
 function applyStyles(styles, text) {
-
-    let styleString = styles.map(style => `${style.name}: ${style.value}`).join("; ");
-
-    document.write(`<h2 style="${styleString}">${text}</h2>`);
+    let styleString = styles.map(style => style.join(": ")).join("; ");
+    
+    document.write(`<p style="${styleString}">${text}</p>`);
 }
 
-applyStyles(styles, "Hello, world!");
+let styles = [
+    ["color", "blue"],
+    ["font-size", "18px"],
+    ["text-align", "center"],
+    ["text-decoration", "underline"]
+];
+
+let text = "Hello, Stylish World!";
+
+applyStyles(styles, text);
